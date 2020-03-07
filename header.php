a305bad20236b66553c43b6bb86f79c33ea68b99
fortune-web <daniel.wong9503@gmail.com>
2020-03-07 16:56:25 +0800

    pagetitle

--- header.php
+++ header.php
@@ -4,7 +4,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
-	<title><?php bloginfo( 'name' ); ?></title>
+	<title>
+    <?php if(is_front_page() || is_home()){
+        echo get_bloginfo('name');
+    } else{
+        echo wp_title(''), " &raquo; ", get_bloginfo('name');
+    }?>
+	</title>
 </head>
 
 <body <?php body_class(); ?>>
