1228b4b510c3eeb1528d0f56cea7e1f407c6728a
fortune-web <daniel.wong9503@gmail.com>
2020-03-09 10:02:12 +0800

    archive link

--- footer.php
+++ footer.php
@@ -2,8 +2,9 @@
       <strong>© 2020</strong>
       <div class="right">
       	<?php if(!is_front_page() || !is_home()){
-       		echo "<a href='/'>Главная</a>";
-    	}?>      	
+       		echo "<a href='/'>Главная</a> /";
+    	}?>
+    	<a href='/archive/'>Архив</a>
       </div>
 </div>
 </body>
