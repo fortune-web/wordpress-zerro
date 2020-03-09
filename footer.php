01b334f00f300d7ea34c309999cfdef4d731beb1
fortune-web <daniel.wong9503@gmail.com>
2020-03-09 08:54:03 +0800

    not show main link if frontpage

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <div class="footer">
       <strong>© 2020</strong>
       <div class="right">
-      	<a href="/">Главная</a>
+      	<?php if(!is_front_page() || !is_home()){
+       		echo "<a href='/'>Главная</a>";
+    	}?>      	
       </div>
 </div>
 </body>
