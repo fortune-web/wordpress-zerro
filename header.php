616d2f20ac5e471c131e14a70969965fb3081ae0
fortune-web <daniel.wong9503@gmail.com>
2020-03-10 09:25:35 +0800

    rss link

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+	<link rel="alternate" type="application/rss+xml" title='<?php echo get_bloginfo('name'); ?>' href='<?php bloginfo('rss2_url'); ?>' />
 	<title>
     <?php if(is_front_page() || is_home()){
         echo get_bloginfo('name');
