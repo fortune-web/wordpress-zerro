633659079b5b6199833692b6b317c27805856aaf
fortune-web <daniel.wong9503@gmail.com>
2020-03-15 19:52:22 +0800

    update personal contacts

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
   <br /><br />
   <u>Социалки:</u><br />
   > <a href="https://t.me/ru_3err0" target="_blank">Telegram</a> / <a href="https://github.com/3err0" target="_blank">Github</a> / <a href="https://vk.com/3err0" target="_blank">VK</a>
+   <br /><br />
+  <u>Контакты для связи:</u><br />
+  > +79246194656 / <a href="mailto:mail@3err0.ru?subject=Hi" target="_blank">mail@3err0.ru</a>
 </span>
     </p>
     </span>
